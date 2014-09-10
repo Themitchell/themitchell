316ed7cbb6ee9828079840dd877697a683ed017c
Andrew Mitchell <themitchell@me.com>
2014-09-10 12:25:27 +0100

    Introduce Rspec configs

--- config/application.rb
+++ config/application.rb
@@ -26,5 +26,12 @@ module Themitchell
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+    #
+    config.generators do |g|
+      g.test_framework :rspec,
+        fixture: true,
+        views: false,
+        fixture_replacement: :factory_girl
+    end
   end
 end
