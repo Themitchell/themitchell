2dc2be20a077318ba0b133bf2940d42bed3435f7
Andy Mitchell <themitchell@me.com>
2015-07-24 13:57:04 +0100

    Upload images in retina and 1x versions

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -1,51 +1,36 @@
 # encoding: utf-8
 
 class ImageUploader < CarrierWave::Uploader::Base
+  include CarrierWave::MiniMagick
 
-  # Include RMagick or MiniMagick support:
-  # include CarrierWave::RMagick
-  # include CarrierWave::MiniMagick
-
-  # Choose what kind of storage to use for this uploader:
   storage :file
   # storage :fog
 
-  # Override the directory where uploaded files will be stored.
-  # This is a sensible default for uploaders that are meant to be mounted:
-  def store_dir
-    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+  process convert: 'png'
+
+  version :default_x2 do
+    process resize_to_limit: [1920, 1188]
   end
 
-  # Provide a default URL as a default if there hasn't been a file uploaded:
-  # def default_url
-  #   # For Rails 3.1+ asset pipeline compatibility:
-  #   # ActionController::Base.helpers.asset_path("fallback/" + [version_name, "default.png"].compact.join('_'))
-  #
-  #   "/images/fallback/" + [version_name, "default.png"].compact.join('_')
-  # end
+  version :default_x1, from_version: :default_x2 do
+    process scale: 0.5
+  end
 
-  # Process files as they are uploaded:
-  # process :scale => [200, 300]
-  #
-  # def scale(width, height)
-  #   # do something
-  # end
-
-  # Create different versions of your uploaded files:
-  # version :thumb do
-  #   process :resize_to_fit => [50, 50]
-  # end
-
-  # Add a white list of extensions which are allowed to be uploaded.
-  # For images you might use something like this:
   def extension_white_list
     %w(jpg jpeg gif png)
   end
 
-  # Override the filename of the uploaded files:
-  # Avoid using model.id or version_name here, see uploader/store.rb for details.
-  # def filename
-  #   "something.jpg" if original_filename
-  # end
+  def store_dir
+    "uploads/#{model.class.to_s.underscore}/#{mounted_as}"
+  end
 
+  def full_filename(for_file)
+    # format exmaple: '1-original.png', '1-x1.png'
+    "#{model.id}-#{version_name || 'original'}.png"
+  end
+
+  def scale(multiplier)
+    dimensions = ::MiniMagick::Image.open(file.file)[:dimensions]
+    resize_to_fill(dimensions[0] * multiplier, dimensions[1] * multiplier)
+  end
 end
